c0014550b15169f7d0d042577187f07654b99c24
Srinivas Chatla <srinivas.chatla@gmail.com>
2023-09-13 23:09:08 +0530

    Login and Browse

---
+++ app/src/main/java/com/example/kotlinlogin/Browse.kt
@@ -0,0 +1,38 @@
+package com.example.kotlinlogin
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Browse : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browse)
+
+        val amazonval = findViewById<Button>(R.id.button2)
+        val flipkartval = findViewById<Button>(R.id.button3)
+        val googleval = findViewById<Button>(R.id.button4)
+        val toival = findViewById<Button>(R.id.button5)
+        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+
+        amazonval.setOnClickListener {
+            openURL.data = Uri.parse("https://www.amazon.in/")
+            startActivity(openURL)
+        }
+        flipkartval.setOnClickListener {
+            openURL.data = Uri.parse("https://www.flipkart.com/")
+            startActivity(openURL)
+        }
+        googleval.setOnClickListener {
+            openURL.data = Uri.parse("https://www.google.com/")
+            startActivity(openURL)
+        }
+        toival.setOnClickListener {
+            openURL.data = Uri.parse("https://m.timesofindia.com/")
+            startActivity(openURL)
+        }
+
+    }
+}
--- app/src/main/java/com/example/kotlinlogin/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlogin/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.kotlinlogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            val submitval = findViewById<Button>(R.id.button)
+            submitval.setOnClickListener {
+                val intent1 = Intent(this, Browse::class.java)
+                startActivity(intent1)
+            }
+
+        }
 }
